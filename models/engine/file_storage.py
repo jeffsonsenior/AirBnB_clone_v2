7538a2d2cd7472fe9921ac84fde855abc6c454bf
jeffsonsenior <kinyuajeff@gmail.com>
2024-06-29 20:31:36 +0000

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,12 +66,10 @@ class FileStorage:
 
     def delete(self, obj=None):
         """
-         Delete obj from __objects if it’s inside - if obj is equal to None,
-           the method should not do anything
-        """
+         Delete an object"""
         if obj is None:
             return
-        obj_to_del = f"{obj.__class__.__name__}.{obj.id}"
+        obj_to_del = obj.__class__.__name__+ "." + obj.id
 
         try:
             del FileStorage.__objects[obj_to_del]
